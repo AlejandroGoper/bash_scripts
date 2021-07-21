e5f7278e696c0e5073f54d6f9989b157abe7b370
AlejandroGoper <ia.gomezperez@ugto.mx>
2021-07-21 14:34:07 -0500

    Comprension de los comandos de consola puestos en un script

---
+++ script_comando_consola.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# =================================
+#    Comandos de consola basicos
+# Autor: I. Alejandro Gomez
+# =================================
+
+
+
+# Imprime el directorio actual
+pwd
+
+# Damos instrucciones como si estuviesemos en la terminal
+# Aqui compilamos un programa
+gcc holamundo.c -o ejecutable
+# Ejecutamos el programa
+./ejecutable
+
+# Imprimimos por pantalla la fecha actual
+date
+
+
