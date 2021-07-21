d955ba72cbe9e8bb439e00c0bc11cdcc2e25d26b
AlejandroGoper <ia.gomezperez@ugto.mx>
2021-07-21 14:00:58 -0500

    Declaracion de variables y sintaxis

---
+++ variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# ==================================
+#  Script para manejo de variables
+# Autor: I. Alejandro Gomez
+# ==================================
+
+variable='Hola mundo'
+echo $variable
+variable=5.46
+echo $variable
